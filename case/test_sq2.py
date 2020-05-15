38afb074e4e2c5cf5a9b39b990be47f930f53a7f
suxiangpeng <suxiangpeng@kingsoft.com>
2020-05-15 12:22:30 +0800

    自动化测试代码修改

---
+++ case/test_sq2.py
@@ -0,0 +1,20 @@
+from case.url import *
+from common.req import *
+import pytest
+
+path = '/sq2'
+
+class Test_sq2:
+    @pytest.mark.sq
+    def test_check_sq1(self):
+       url = url_sql_test + path
+       res = req.r_get(url).text
+       assert  res == "ok"
+
+
+
+
+
+
+
+
---
+++ case/test_sql.py
@@ -0,0 +1,3 @@
+
+class Test_sql:
+    def test_sql_1(self):
